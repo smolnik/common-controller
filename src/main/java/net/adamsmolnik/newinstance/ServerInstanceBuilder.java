c1d2ef3dfffda886e5b1faa56196d4c197c0f072
smolnik <smolnik@wp.pl>
2014-09-09 14:40:01 +0200

    first classes

---
+++ src/main/java/net/adamsmolnik/newinstance/ServerInstanceBuilder.java
@@ -0,0 +1,12 @@
+package net.adamsmolnik.newinstance;
+
+
+/**
+ * @author ASmolnik
+ *
+ */
+public interface ServerInstanceBuilder<T extends SetupParamsView, R extends ServerInstance> {
+
+    R build(T spv);
+
+}
